37dfbeba43e3356f8f057fa2ec0bb448f93d0723
Mario Lasluisa <mjlasca@gmail.com>
2025-04-17 19:27:21 -0500

    test 6

--- app/Http/Controllers/PropuestasControllerV2.php
+++ app/Http/Controllers/PropuestasControllerV2.php
@@ -634,10 +634,10 @@ class PropuestasControllerV2 extends Controller
             DB::enableQueryLog(); // Activa el log
             $ids = array_map('intval', explode(",", $req["asegurados"]));
 
-            $insureds = cliente::whereIn('id', $ids)
+            $insureds = cliente::whereIn('reg', $ids)
                 ->where('codestado', 1)
-                ->groupBy('id')
-                ->orderBy('id', 'ASC')
+                ->groupBy('reg')
+                ->orderBy('reg', 'ASC')
                 ->get();
 
             dump(DB::getQueryLog());
