b31cf5240c8f44e63d178995c7d0e011446eb02a
Mario Lasluisa <mjlasca@gmail.com>
2025-05-19 18:57:24 -0500

    create client

--- app/Models/cliente.php
+++ app/Models/cliente.php
@@ -7,7 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class cliente extends Model
 {
-    protected $fillable = ['reg','id','nombres','apellidos','tipo_id','fecha_nacimiento','codempresa','codestado','ultmod'];
+    protected $fillable = [
+        'reg',
+        'id',
+        'nombres',
+        'apellidos',
+        'tipo_id',
+        'fecha_nacimiento',
+        'codempresa',
+        'telefono',
+        'direccion',
+        'email',
+        'codpostal',
+        'sexo',
+        'codestado',
+        'ultmod'];
     use HasFactory;
 
     public function propuestas(){
