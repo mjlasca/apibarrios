9ab7abfedbf87824b5b393e4418f385df1581e4c
Mario Lasluisa <mjlasca@gmail.com>
2026-07-26 22:40:52 -0500

    cuit pagador en consolidado

--- app/Http/Controllers/PropuestasControllerV2.php
+++ app/Http/Controllers/PropuestasControllerV2.php
@@ -568,7 +568,7 @@ class PropuestasControllerV2 extends Controller
             }
         }
 
-        $reportRest = Propuesta::select("id","reg","documento","nombre","num_polizas","meses","id_cobertura","id_barrio","nueva_poliza","premio","premio_total","fechaDesde","fechaHasta","clausula","barrio_beneficiario","ultmod","useredit as user_edit","codestado","cobertura_suma","cobertura_deducible","cobertura_gastos","promocion","paga","fecha_paga","referencia","prima","master","organizador","productor","puntodeventa","prefijo","updated_at","created_at","formadepago","usuariopaga","tipopago","compformadepago as compformapago","csrf","fecha_nacimiento","codempresa","idpropuesta","nota","data_barrios","version","valor_pagado","imputacion","fecha_comprobante")->where('fecha_paga','>',$date.' 00:00:01')
+        $reportRest = Propuesta::select("id","reg","documento","nombre","num_polizas","meses","id_cobertura","id_barrio","nueva_poliza","premio","premio_total","fechaDesde","fechaHasta","clausula","barrio_beneficiario","ultmod","useredit as user_edit","codestado","cobertura_suma","cobertura_deducible","cobertura_gastos","promocion","paga","fecha_paga","referencia","prima","master","organizador","productor","puntodeventa","prefijo","updated_at","created_at","formadepago","usuariopaga","tipopago","compformadepago as compformapago","csrf","fecha_nacimiento","codempresa","idpropuesta","nota","data_barrios","version","valor_pagado","imputacion","fecha_comprobante", "cuit_pagador")->where('fecha_paga','>',$date.' 00:00:01')
                                 ->where('fecha_paga','<',$date.' 23:59:59')
                                 ->where('codempresa',$codempresa)
                                 ->get();
